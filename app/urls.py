19a2d7024a83f3f6232a6e0948926043ae23db26
Aldrich Heinz Alvarez <aldrichheinzalvarez@gmail.com>
2021-03-23 16:28:48 +0800

    commit

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.home, name="app-home"),
+    path('', views.home, name="app-home"),
     path('about/', views.about, name="app-about"),
 ]
