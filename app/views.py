940a94e295838b9aa7313549ae33815a6ce3d7d9
Aldrich Heinz Alvarez <aldrichheinzalvarez@gmail.com>
2021-03-24 11:16:22 +0800

    update

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+ 
 posts = [
    {
       'image': 'https://wallpaperaccess.com/full/3389575.jpg',
       'author': 'Sean Darrel Hernandez',
       'class': 'Photoshop Master Class',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
       'likes': '1000',
       'ranking': '1'
    },
@@ -14,7 +15,7 @@ posts = [
       'image': 'https://wallpaperaccess.com/full/3389575.jpg',
       'author': 'Aldrich Heinz Alvarez',
       'class': 'Full Stack Web Dev using Django',
-      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
       'likes': '1000',
       'ranking': '2'
    },
