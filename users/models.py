ddb509aecbd8b5f6e0a90b9489ae3b5a1bc8b807
Aldrich Heinz Alvarez <aldrichheinzalvarez@gmail.com>
2021-03-28 14:43:45 +0800

    update

--- users/models.py
+++ users/models.py
@@ -5,8 +5,8 @@ from PIL import Image
 class Profile(models.Model):
    user = models.OneToOneField(User, on_delete=models.CASCADE)
    bio = models.CharField(max_length=500, default='Update profile to add bio..', blank=True)
-   image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-   cover = models.ImageField(default='default.jpg', upload_to='cover_pics')
+   image = models.ImageField(default='default_profile.jpg', upload_to='profile_pics')
+   cover = models.ImageField(default='default_cover.png', upload_to='cover_pics')
 
    def __str__(self):
       return f'{self.user.username} Profile'
