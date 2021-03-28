f427aa172e91763b32cbbca02af8f4aabc901a90
Aldrich Heinz Alvarez <aldrichheinzalvarez@gmail.com>
2021-03-28 14:00:41 +0800

    commit

--- osed/settings.py
+++ osed/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, CreateBioForm
 
 def register(request):
    if request.method == 'POST':
@@ -10,7 +10,7 @@ def register(request):
          form.save()
          username = form.cleaned_data.get('username')
          messages.success(request, f'Your account has been created {username}! You are now able to login')
-         return redirect('login')
+         return redirect('app-home')
    else:
       form = UserRegisterForm()
    return render(request, 'users/register.html', {'form': form})
@@ -18,3 +18,28 @@ def register(request):
 @login_required
 def profile(request):
    return render(request, 'users/profile.html')
+
+def update(request):
+   if request.method == 'POST':
+      u_form = UserUpdateForm(request.POST, instance=request.user)
+      b_form = CreateBioForm(request.POST, instance=request.user.profile)
+      p_form = ProfileUpdateForm(request.POST, 
+                                 request.FILES, 
+                                 instance=request.user.profile)
+      if u_form.is_valid() and b_form.is_valid() and p_form.is_valid():
+         u_form.save()
+         b_form.save()
+         p_form.save()
+         return redirect('profile')
+         
+   else:
+      u_form = UserUpdateForm(instance=request.user)
+      b_form = CreateBioForm(instance=request.user.profile)
+      p_form = ProfileUpdateForm(instance=request.user.profile)
+      
+   context = {
+      'u_form': u_form,
+      'b_form': b_form,
+      'p_form': p_form
+   }
+   return render(request, 'users/update.html', context)
